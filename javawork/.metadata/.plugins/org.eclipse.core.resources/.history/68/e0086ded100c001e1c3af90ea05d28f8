0df4afefcd498d9af4377f8f604e6abf165f56dd
ParkHC <luis1018@naver.com>
2023-06-19 17:37:31 +0900

    master

---
+++ javawork/.metadata/.plugins/org.eclipse.core.resources/.history/68/e0086ded100c001e1c3af90ea05d28f8
@@ -0,0 +1,10 @@
+package day0616;
+
+public class Book69 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ javawork/MyInfo.java
@@ -0,0 +1,10 @@
+class MyInfo{
+	public static void main(String [] args){
+		System.out.println("==========");
+		System.out.println("학생 개인정보");
+		System.out.println("==========");
+		System.out.println("전공: " + args[0] + "학과");
+		System.out.println("학년: " +args[1] + "학년");
+	}
+
+}
